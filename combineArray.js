0605f478b95d4adcd2e461927b9bdfd30edc883e
Anthony Kuang <AnthonyKuang116@gmail.com>
2022-01-18 01:43:31 -0500

    added combineArray

---
+++ combineArray.js
@@ -0,0 +1,32 @@
+let first = [
+    { uuid: 2, name: "Darling" },
+    { uuid: 3, name: "Cry-Baby" },
+    { uuid: 4, name: "Snitch" },
+    { uuid: 5, name: "Pawn" }
+];
+
+let second = [
+    { uuid: 1, role: "admin" },
+    { uuid: 2, role: "contributor" },
+    { uuid: 3, role: "owner" },
+    { uuid: 4, role: "contributor" }
+];
+
+
+
+let newArr = [];
+
+for(let i = 0; i < first.length; i++) {
+    newArr.push({...first[i], ...(second.find((item) => item.uuid === first[i].uuid))})
+}
+// console.log(newArr);
+
+
+const mergeArr = (first, second) => {
+   return first.map(item => ({
+        ...second.find((info) => (info.uuid === item.uuid)),
+        ...item
+    }))
+}
+console.log(mergeArr(first, second));
+
